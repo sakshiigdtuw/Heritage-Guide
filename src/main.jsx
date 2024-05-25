7578923c8bb3d8ae853998b60d79a57a060f977e
vishal0316 <vr987195@gmail.com>
2024-05-26 01:14:28 +0530

    main jsx fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
 import ContextProvider from "./Context/Context.jsx";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ContextProvider>
