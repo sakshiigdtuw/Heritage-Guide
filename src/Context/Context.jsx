5d3540fbeb64c87cc4a46d550e273829dd29b4a6
vishal Rajput <vr987195@gmail.com>
2024-05-26 02:45:01 +0530

    Update Context.jsx

--- src/Context/Context.jsx
+++ src/Context/Context.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 import { createContext, useState } from "react";
-import run from "../Config/gemini";
+import run from "../Config/gemini.js";
 
 export const Context = createContext();
 
